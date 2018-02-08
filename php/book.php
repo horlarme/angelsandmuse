ba5644666062fffbe7ea5c01da866e9afe1b6520
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2018-02-08 16:45:11 +0100

    Booking

--- php/book.php
+++ php/book.php
@@ -48,8 +48,7 @@ if (get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-//$address = "info@angelsandmuse.com";
-$address = "tyxupej@amail.club";
+$address = "info@angelsandmuse.com";
 
 
 // Configuration option.
